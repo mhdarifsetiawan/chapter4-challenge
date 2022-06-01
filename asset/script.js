ebe27c5a0d11a8e0c40288c747e6121bcb7dfd1d
mhdarifsetiawan <ariefroyal@gmail.com>
2022-06-02 00:07:41 +0700

    menambahkan fungsi delet element

--- asset/script.js
+++ asset/script.js
@@ -1,14 +1,36 @@
-const button = document.getElementById("add");
+const addElement = document.getElementById("add");
+const deleteElement = document.getElementById("delete");
 const paragraf1 = document.getElementById("p1");
 
-let numId = 1;
-button.onclick = function () {
+let elementId = 1;
+
+// Add Element
+addElement.onclick = function () {
   const parent = document.getElementById("wrapper");
-
-  
-
   let clonedElement = paragraf1.cloneNode(true);
-  clonedElement.id = "p" + (numId + 1);
+
+  clonedElement.id = "p" + (elementId + 1);
   parent.append(clonedElement);
-  ++numId;
+  ++elementId;
+  // console.log(elementId);
 };
+
+// Delet Element
+// deleteElement.onclick = function () {
+//   const parent = document.getElementById("wrapper");
+//   parent.removeChild(parent.lastElementChild);
+//   --elementId;
+//   console.log(elementId);
+// };
+
+deleteElement.onclick = function () {
+  const parent = document.getElementById("wrapper");
+  if (elementId > 1) {
+    parent.removeChild(parent.lastElementChild);
+    --elementId;
+    // console.log(elementId);
+  } else {
+    alert("Jangan dihapus semua donk say");
+  }
+};
+
